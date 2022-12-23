810aa22599d92aba3fe51533582f41b94c924e49
William Bourret <46964762+bourret27@users.noreply.github.com>
2022-12-23 15:15:24 -0500

    Created direct hit

--- proxy_app.py
+++ proxy_app.py
@@ -1,30 +1,54 @@
 from sshtunnel import SSHTunnelForwarder
 import pymysql
 import pandas as pd
+import argparse
+
+cluster_hosts = ['172.31.2.2', '172.31.2.3', '172.31.2.4', '172.31.2.5']
+str_query = 'SELECT * FROM actor;'
 
 def create_ssh_tunnel():
+    print('Creating tunnel...')
+    
     tunnel = SSHTunnelForwarder(
-        ('172.31.2.3', 22),
+        (cluster_hosts[0], 22),
         ssh_username="ubuntu",
         ssh_pkey="labsuser.pem",
         local_bind_address=('127.0.0.1', 3306),
         remote_bind_address=('127.0.0.1', 3306)
     )
     tunnel.start()
+    
+    print('Tunnel creation successful!')
 
     return tunnel
 
-if __name__ == "__main__":
-    tunnel = create_ssh_tunnel()
-    str_query = 'SELECT * FROM actor;'
-    conn = pymysql.connect(
-        host='172.31.2.3',
+def create_connection_to_db(hostname):
+    connection = pymysql.connect(
+        host=hostname,
         user='root',
         password='root',
         db="sakila",
         port=3306
     )
 
-    data = pd.read_sql_query(str_query, conn)
+    return connection
 
+def run_direct_hit():
+    tunnel = create_ssh_tunnel()
+    connection = create_connection_to_db(cluster_hosts[0])
+    data = pd.read_sql_query(str_query, connection)
     print(data)
+
+    return {}
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Proxy application')
+    parser.parser.add_argument('-p', required=True, choices=['direct', 'random', 'custom'])
+    args = vars(parser.parse_args())
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    if args['p'] == 'direct':
+        run_direct_hit()
