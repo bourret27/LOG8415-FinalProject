9626a80286ca18c5f593297a36abf2a299cec332
William Bourret <46964762+bourret27@users.noreply.github.com>
2022-12-23 14:41:19 -0500

    Added query to proxy app.

--- proxy_app.py
+++ proxy_app.py
@@ -1,5 +1,6 @@
 from sshtunnel import SSHTunnelForwarder
 import pymysql
+import pandas as pd
 
 if __name__ == "__main__":
     tunnel = SSHTunnelForwarder(
@@ -10,4 +11,15 @@ if __name__ == "__main__":
         remote_bind_address=('127.0.0.1', 3306)
     )
     tunnel.start()
+
+    str_query = 'SELECT * FROM actor;'
+    conn = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='root',
+        db="sakila",
+        port=3306
+    )
+
+    data = pd.read_sql_query(str_query, conn)
     print("Hello World!")
