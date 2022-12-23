28d9e9deb9aa5c69de42aac89d10a539e11d00b8
William Bourret <46964762+bourret27@users.noreply.github.com>
2022-12-23 14:26:28 -0500

    Fixed typo in proxy app.

--- proxy_app.py
+++ proxy_app.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     tunnel = SSHTunnelForwarder(
         ('172.31.2.2', 22),
         ssh_username="ubuntu",
-        ssh_key="labsuser.pem",
+        ssh_pkey="labsuser.pem",
         local_bind_address=('127.0.0.1', 3306),
         remote_bind_address=('127.0.0.1', 3306)
     )
